570cbe8b15191582d8409fd7eaa4775ce6c2a5dd
nileshp-07 <patidarnilesh7223@gmail.com>
2024-03-25 16:03:21 +0530

    resolved error of react-circular-progressbar

--- frontend/src/components/core/productDetails.jsx/Reviews.jsx
+++ frontend/src/components/core/productDetails.jsx/Reviews.jsx
@@ -3,7 +3,7 @@ import {
   CircularProgressbar,
   buildStyles,
 } from "react-circular-progressbar";
-// import "react-circular-progressbar/dist/styles.css";
+import "react-circular-progressbar/dist/styles.css";
 import RatingStars from "../../common/RatingStars"
 import ProgressBar from "@ramonak/react-progress-bar";
 import {TiStarFullOutline} from "react-icons/ti"
