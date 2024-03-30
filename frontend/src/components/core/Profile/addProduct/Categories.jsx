bfd24c665011b6381b6b6dd4cf88295e5fa1d070
nileshp-07 <patidarnilesh7223@gmail.com>
2024-03-30 15:39:26 +0530

    AddProduct: Categories

--- frontend/src/components/core/Profile/addProduct/Categories.jsx
+++ frontend/src/components/core/Profile/addProduct/Categories.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import { RiArrowRightSLine } from "react-icons/ri";
+import { RxCross2 } from "react-icons/rx";
+import { FaArrowRightLong } from "react-icons/fa6";
+import {toast} from "sonner"
+
 
 
 
@@ -312,28 +316,50 @@ const Categories = () => {
   const [mainCategory, setMainCategory] = useState(categories[0]);
   const [selectedCategories , setSelectedCategories] = useState([]);
 
+  console.log(selectedCategories);
+  const changeHandler = (e) => {
+      const category = e.target.value;
+
+      if(selectedCategories.includes(category))
+      {
+        const newSelectedCategories = [...selectedCategories];
+        newSelectedCategories.splice(selectedCategories.indexOf(category), 1);
+        setSelectedCategories(newSelectedCategories);
+      }
+      else{
+          if(selectedCategories.length === 3)
+          {
+            toast.error("Max category limit exceeds");
+            e.target.checked = false;
+            return;
+          }
+         setSelectedCategories([...selectedCategories, category])
+      }
+  }
+
   console.log(mainCategory)
   return (
-    <div>
+    <div className='-mt-2'>
         
         <h2 className='font-medium'>
           Select the category your product belong to (max. 3)
         </h2>
 
-        <div className='flex gap-16 my-10 bg-gray-50 rounded-md p-6 max-h-[500px] overflow-hidden'>
-           <div className='flex flex-col gap-1 overflow-x-auto w-[260px]'>
+        <div className='flex gap-16 my-5 bg-gray-50 rounded-md p-6 h-[400px] overflow-hidden'>
+           <div className='flex flex-col gap-1 overflow-x-auto w-[250px]'>
               {
                  categories.map((category , index) => (
                    <div 
                       onClick={() => setMainCategory(category)}
-                      key={index} className={`flex justify-between items-center rounded-md px-5 py-2  cursor-pointer ${mainCategory.name  === category.name && "bg-royal-blue-100" }`}>
+                      key={index} 
+                      className={`flex justify-between items-center rounded-md px-5 py-2 w-full  cursor-pointer ${mainCategory.name  === category.name && "bg-royal-blue-100" }`}>
                           <p className='text-[17px]'>{category.name}</p>
                           <RiArrowRightSLine/>
                    </div>
                  ))
               }
            </div>
-           <div className='grid gap-12 grid-cols-3 overflow-x-auto '  style={{ marginRight: '-17px' }}>
+           <div className='grid gap-x-12 gap-y-8 grid-cols-3 overflow-x-auto ' >
                {
                   mainCategory.subcategories1.map((subcategories1, index1) => (
                     <div key={index1}>
@@ -341,12 +367,14 @@ const Categories = () => {
                        <div className='flex flex-col gap-1'>
                         {
                            subcategories1.subcategories2.map((subcategories2, index2) => (
-                              <div className='flex gap-2 cursor-pointer'>
+                              <div className='flex gap-2 cursor-pointer' key={index2}>
                                  <input
                                    type='checkbox'
                                    name={subcategories1.name}
                                    id={subcategories2.name}
+                                   checked = {selectedCategories.includes(subcategories2.name)}
                                    value={subcategories2.name}
+                                   onChange={changeHandler}
                                  />
                                  <label htmlFor={subcategories2.name} className='cursor-pointer'>{subcategories2.name}</label>
                               </div>
@@ -358,18 +386,38 @@ const Categories = () => {
                }
            </div>
         </div>
-        <div>
-           <p>
-             Selected categories:
-           </p>
-           <div>
-
-           </div>
+        {
+           selectedCategories.length > 0 && (
+            <div className='flex gap-5'>
+                <p className='font-medium'>
+                  Selected categories:
+                </p>
+                <div className='flex gap-2'>
+                    {
+                      selectedCategories.map((category, index) => (
+                        <div key={index} className='flex justify-between gap-3 items-center py-[2px] pl-5 pr-2 rounded-full bg-royal-blue-100 w-fit'>
+                          <p className='font-medium text-[14px]'>{category}</p>
+                          <div className='cursor-pointer'
+                                onClick={() => {
+                                  const newSelectedCategories = [...selectedCategories];
+                                  newSelectedCategories.splice(index, 1);
+                                  setSelectedCategories(newSelectedCategories);
+                                }}>
+                            <RxCross2/>
+                          </div>
+                        </div>
+                      ))
+                    }
+                </div>
+            </div>
+           )
+        }
+        <div className='my-9 mx-auto flex justify-end px-16'>
+            <button className=' flex items-center gap-2 py-1 pt-2 px-5 bg-royal-blue-500 rounded-md text-white'>
+                <p className='mb-1'>Next</p>
+                <FaArrowRightLong/>
+            </button>
         </div>
-
-        <button>
-           Next
-        </button>
 
     </div>
   )
