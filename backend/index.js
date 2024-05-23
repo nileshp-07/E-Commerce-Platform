012fb1902798af538385f2ef0637350f75e708f9
nileshp-07 <patidarnilesh7223@gmail.com>
2024-05-23 21:18:17 +0530

    #idk

--- backend/index.js
+++ backend/index.js
@@ -57,7 +57,7 @@ app.use(session({
 
 app.post('/webhook', express.raw({ type: 'application/json' }),(request, response) => {
   const sig = request.headers['stripe-signature'];
-  consolo.log("testing....");
+  console.log("testing....");
 
 
   let event;
